0b6fba4145c5ad8ee42d55bf779b8477270a3e38
Emma Lin <e33lin@uwaterloo.ca>
2023-01-17 23:30:15 -0500

    added database

---
+++ smart-cart/db/migrate/20230118042155_create_lists.rb
@@ -0,0 +1,10 @@
+class CreateLists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lists do |t|
+      t.integer :list_id
+      t.string :item
+
+      t.timestamps
+    end
+  end
+end
