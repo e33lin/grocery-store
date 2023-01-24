b22d3bfc15de9307a36ae84a3625ab6e5fe1c05a
Jillian Exconde <jillianexconde@Jillians-MacBook-Air.local>
2023-01-23 19:11:58 -0500

    moved stuff to root

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :lists
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "lists#index"
+
+  get "home", to: "home#show"
+  get "stores", to: "stores#show"
+end
