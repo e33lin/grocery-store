50906813d8598b8c0646b6e861aaac67e9c48d16
Jillian Exconde <jillianexconde@Jillians-MacBook-Air.local>
2023-02-13 15:15:24 -0500

    install pip

--- smart-cart/bin/render-build.sh
+++ smart-cart/bin/render-build.sh
@@ -7,6 +7,7 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 
+python3 -m pip install --upgrade pip
 pip install virtualenv
 virtualenv venv
 source venv/bin/activate
