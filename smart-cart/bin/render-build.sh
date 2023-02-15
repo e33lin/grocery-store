d7cb2be3c51167fd0d1d2d2bd87fffdeb74876d8
Caitlan Krasinski <ckrasinski@live.ca>
2023-02-15 17:13:01 -0500

    python

--- smart-cart/bin/render-build.sh
+++ smart-cart/bin/render-build.sh
@@ -7,6 +7,9 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 
-source env/bin/activate
-env/bin/pip3 install -r backend/requirements.txt
+python3 --version
+pip3 --version
+
+# source env/bin/activate
+# env/bin/pip3 install -r backend/requirements.txt
 #source .env/bin/activate && pip install -r backend/requirements.txt
