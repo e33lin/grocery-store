5627f75bd602ff4b39354f28127b2749133522be
Emma Lin <e33lin@uwaterloo.ca>
2023-02-15 15:31:09 -0500

    procfile

--- smart-cart/bin/render-build.sh
+++ smart-cart/bin/render-build.sh
@@ -7,6 +7,6 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 
-#source env/bin/activate
+source env/bin/activate
 #env/bin/pip3 install -r backend/requirements.txt
-source .env/bin/activate && pip install -r backend/requirements.txt
+#source .env/bin/activate && pip install -r backend/requirements.txt
