ae733c9430516cd73d7a159387b9361a1fe8ec81
Emma Lin <e33lin@uwaterloo.ca>
2023-02-15 15:09:01 -0500

    idk

--- smart-cart/bin/render-build.sh
+++ smart-cart/bin/render-build.sh
@@ -9,4 +9,4 @@ bundle exec rake db:migrate
 
 #source env/bin/activate
 #env/bin/pip3 install -r backend/requirements.txt
-virtualenv .env && source .env/bin/activate && pip install -r backend/requirements.txt
+source .env/bin/activate && pip install -r backend/requirements.txt
