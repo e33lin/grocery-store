2b698f5271e676682c6cff6c0127c136f2b8f8ca
Caitlan Krasinski <ckrasinski@live.ca>
2023-02-15 19:16:34 -0500

    path in python script

--- smart-cart/backend/search_v3.py
+++ smart-cart/backend/search_v3.py
@@ -9,6 +9,12 @@ EX: python search_v3.py "['2% milk', 'Cheddar Cheese', 'white sliced bread']" 2
 grocery_list must be passed with " " around the actual list object 
 
 '''
+
+import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
  
 import pandas as pd
 import numpy as np
@@ -16,7 +22,6 @@ from nltk.stem import PorterStemmer
 from nltk.metrics.distance import jaccard_distance
 from fuzzywuzzy import process
 import time
-import sys
 import ast
 from pickle import load
 import json 
--- smart-cart/bin/render-build.sh
+++ smart-cart/bin/render-build.sh
@@ -9,11 +9,6 @@ bundle exec rake db:migrate
 
 export PATH="/opt/render/.local/bin:$PATH"
 
-
-# python3 --version
-# pip3 --version
-
-pip3 uninstall -y -r backend/requirements.txt
 pip3 install -r backend/requirements.txt
 
 # source env/bin/activate
