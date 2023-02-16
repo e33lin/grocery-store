36d0151420f9b34f90779918fd49306adcc02292
Caitlan Krasinski <ckrasinski@live.ca>
2023-02-16 13:29:08 -0500

    fingers crossed

--- smart-cart/bin/render-build.sh
+++ smart-cart/bin/render-build.sh
@@ -2,17 +2,17 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate
 
 # export PATH=/opt/render/project/src/smart-cart/
 
-# echo $PATH
+echo $PATH
 # source venv/bin/activate
 # alias python3='/opt/render/project/src/smart-cart/venv/bin/python3'
 
-# which python3 
-# python3 --version
-pip3 install -r backend/requirements.txt 
+which python3 
+python3 --version
+# pip3 install -r backend/requirements.txt 
