e0a8cf26e0a7ecfc2dd0e383aa4e73fc6ba25e74
Caitlan Krasinski <ckrasinski@live.ca>
2023-02-15 19:32:23 -0500

    print

--- smart-cart/backend/search_v3.py
+++ smart-cart/backend/search_v3.py
@@ -12,9 +12,7 @@ grocery_list must be passed with " " around the actual list object
 
 import sys
 import os
-
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(parent_dir)
+print(sys.executable)
  
 import pandas as pd
 import numpy as np
