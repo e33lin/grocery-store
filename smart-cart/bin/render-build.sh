60655397980d3b6f3eb7777620d66050614c64d1
Caitlan Krasinski <ckrasinski@live.ca>
2023-02-15 21:36:30 -0500

    move py files

--- smart-cart/bin/render-build.sh
+++ smart-cart/bin/render-build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-# bundle install
-# bundle exec rake assets:precompile
-# bundle exec rake assets:clean
-# bundle exec rake db:migrate
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
 
 export PATH="/opt/render/.local/bin:$PATH"
 
