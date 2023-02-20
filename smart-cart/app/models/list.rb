425884dcbeaa23e4b99573ad673a8d5a12ebb702
Jillian Exconde <jillianexconde@Jillians-MacBook-Air.local>
2023-02-19 23:19:57 -0500

    added validation to models

--- smart-cart/app/models/list.rb
+++ smart-cart/app/models/list.rb
@@ -1,4 +1,10 @@
 class List < ApplicationRecord
+
+    validates_presence_of :list_id
+    validates_presence_of :item
+    validates_uniqueness_of :item, case_sensitive: false, message: "item already exists in your list"
+    validates_presence_of :quantity
+
     def self.list_as_array(list)
         a = []
         for x in list
--- smart-cart/app/models/recommendation.rb
+++ smart-cart/app/models/recommendation.rb
@@ -1,2 +1,7 @@
 class Recommendation < ApplicationRecord
+    validates_presence_of :list_id
+    validates_presence_of :rec_num
+    validates_presence_of :store
+    validates_presence_of :subtotal
+    validates_presence_of :rec
 end
