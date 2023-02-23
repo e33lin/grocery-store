e049a428395bd8ecc79e93cfe3c4c053588c03a3
Jillian Exconde <jillianexconde@Jillians-MacBook-Air.local>
2023-02-23 17:41:49 -0500

    removed uniqueness line

--- smart-cart/app/models/list.rb
+++ smart-cart/app/models/list.rb
@@ -2,7 +2,6 @@ class List < ApplicationRecord
 
     validates_presence_of :list_id
     validates_presence_of :item
-    validates_uniqueness_of :item, case_sensitive: false, message: "item already exists in your list"
     validates_presence_of :quantity
 
     def self.list_as_array(list)
