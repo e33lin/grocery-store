a8e371c0b1b26ba18079447add9c1a797e335299
Emma Lin <e33lin@uwaterloo.ca>
2023-03-10 22:02:54 -0500

    changed test out for current_user_id

--- smart-cart/app/controllers/home_controller.rb
+++ smart-cart/app/controllers/home_controller.rb
@@ -12,9 +12,9 @@ class HomeController < ApplicationController
             new_session_id = rand(100001) # generates a random number between 0-100000
             if(List.find_by(list_id: new_session_id).blank?)
                 #session[:current_user_id] = new_session_id # sets the environment variable
-                session[:test] ||= new_session_id
-                print "this is the session[:test]"
-                print session[:test]
+                session[:current_user_id] ||= new_session_id
+                print "this is the session[:current_user_id]"
+                print session[:current_user_id]
                 found_unique_id = true # breaks the loop
             end
         end
